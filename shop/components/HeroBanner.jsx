7b3597174fc5e3d5d9d3c35a7b2cf5343ca962be
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-16 19:24:49 +0600

    Basic Hero Banner Set

--- shop/components/HeroBanner.jsx
+++ shop/components/HeroBanner.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">Test</p>
+        <h3>Head Test</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+        <div>
+          <Link href="/product/ID">
+            <button type="button">Button</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+              odio.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
--- shop/pages/index.js
+++ shop/pages/index.js
@@ -4,13 +4,13 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>The best headphone are available here!</p>
       </div>
       <div className="products-container">Products</div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
