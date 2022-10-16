c2c0adb93d99fd2ca74add94e6d641117aa11622
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-16 15:24:18 +0600

    Writing a scratch

--- shop/pages/index.js
+++ shop/pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home </div>;
+  return (
+    <>
+      HeroBanner
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>The best headphone are available here!</p>
+      </div>
+      <div className="products-container">Products</div>
+      Footer
+    </>
+  );
 };
 
 export default Home;
