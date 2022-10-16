defac5d3381586a4c031030b2db8596c2ddbd044
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-16 19:58:25 +0600

    Setups Sanity

--- shop/pages/index.js
+++ shop/pages/index.js
@@ -1,18 +1,37 @@
 import React from "react";
+import { client } from "./../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>The best headphone are available here!</p>
       </div>
-      <div className="products-container">Products</div>
+      <div className="products-container">
+        {products?.map((product) => {
+          product.name;
+        })}
+      </div>
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  // For Product
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  // For Banner
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
